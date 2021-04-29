0dd16ec789a2e8c32100c26a6104fca6bcabc23e
Davidlopez3 <43184290+Davidlopez3@users.noreply.github.com>
2021-04-29 14:08:08 -0500

    mas

---
+++ test-one/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CreateCoolD from './CreateCoolD';
+/* import component.  */
+function App() {
+  return (
+    /* Begin Fragment.  */
+    <>
+    
+    <CreateCoolD />
+
+
+      <input type="text" />
+      <button> Button</button>
+</>
+/* end fragment */
+    );
+}
+
+export default App;
---
+++ test-one/src/CreateCoolD.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CreateCoolD() {
+    return (
+        <div>
+            Buenos Dias
+        </div>
+
+
+
+        /* create  a Component.  */
+    )
+}
